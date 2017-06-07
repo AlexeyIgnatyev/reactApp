065d805016c489f42d86ea2bda9cfe0234758684
Alexey Ignatyev <AlexeyIgnatyev@coherentsolutions.com>
2017-06-07 16:32:18 +0300

    reducer action

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import { connect } from 'react-redux';
 
 class App extends React.Component{
 	
-  render(){
-    return <div>hello world</div>;
-  }
+	greeting(){
+		let phrase = 'hello world';
+		return phrase;
+	}
+
+	addPhrase(){
+		console.log(this.phraseInput.value);
+		this.props.onAddPhrase(this.phraseInput.value);
+		this.phraseInput.value = '';
+	}
+
+	render(){
+		console.log(this.props.testStore);
+		let greet = this.greeting();
+		return (
+			<div>
+				<h3>{ greet }</h3>
+				<div className="phraseForm">
+		   			<input type="text" ref={(input) => { this.phraseInput = input}} />
+		   			<button onClick={ this.addPhrase.bind(this) }>Push phrase</button>
+	   			</div>
+	   		</div>
+		);
+	}
 }
 
-export default App;
+export default connect(
+	state => ({
+		testStore: state
+	}),
+	dispatch => ({
+		onAddPhrase: (phrase) => {
+			dispatch({ type: 'PUSH_DATA', phrase: phrase});
+		}
+	})
+)(App);
--- src/index.js
+++ src/index.js
@@ -6,5 +6,26 @@ import { createStore } from 'redux';
 import App from './App';
 import './css/style.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let initialState = [
+	'yo!',
+	'hey'
+];
 
+function mainApp(state = initialState, action){
+	if (action.type === 'PUSH_DATA') {
+		return [
+			...state,
+			action.phrase
+		];
+	}
+	return state;
+}
+
+let store = createStore(mainApp, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>, 
+	document.getElementById('root')
+);
